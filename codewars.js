b1522af6e82ee1ff7a65ed01de3325010cfd03e2
Jufebrown <jufebrown@gmail.com>
2017-09-06 19:21:47 -0500

    solved camel case kata

---
+++ codewars.js
@@ -0,0 +1,16 @@
+/***********
+Camel Case
+***********/
+
+// Complete the method/function so that it converts dash/underscore
+// delimited words into camel casing. The first word within the output
+// should be capitalized only if the original word was capitalized.
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1)
+
+function toCamelCase(str){
+  let workingArray = str.split(/[\-_]+/)
+  for (var i = 1; i < workingArray.length; i++) {
+    workingArray[i] = capitalizeFirstLetter(workingArray[i])
+  }
+  return workingArray.join('')
+}
