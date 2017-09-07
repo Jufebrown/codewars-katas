0ba792aec742f0fa90dea32cd2b703359ab15527
Jufebrown <jufebrown@gmail.com>
2017-09-06 19:23:32 -0500

    commented out camel case code

--- codewars.js
+++ codewars.js
@@ -5,6 +5,8 @@ Camel Case
 // Complete the method/function so that it converts dash/underscore
 // delimited words into camel casing. The first word within the output
 // should be capitalized only if the original word was capitalized.
+
+/*
 const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1)
 
 function toCamelCase(str){
@@ -14,3 +16,4 @@ function toCamelCase(str){
   }
   return workingArray.join('')
 }
+*/
