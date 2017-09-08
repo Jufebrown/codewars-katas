5fbc5c24cb80dd22313534089a9c10a93f8b64a6
Jufebrown <jufebrown@gmail.com>
2017-09-08 11:00:00 -0500

    started moving zeroes to the end

--- codewars.js
+++ codewars.js
@@ -58,3 +58,14 @@ function duplicateEncode(word){
   }
   return resultArray.join("")
 }
+
+
+/***************
+Moving Zeros To The End
+***************/
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+var moveZeros = function (arr) {
+  // TODO: Program me
+}
