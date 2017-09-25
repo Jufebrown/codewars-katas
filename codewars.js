97e34be80d7df0dcc1a658df4023ce63397dc716
Jufebrown <jufebrown@gmail.com>
2017-09-25 15:57:33 -0500

    added checking car mileage numbers

--- codewars.js
+++ codewars.js
@@ -160,3 +160,193 @@ const solution = (str) => {
 };
 
 solution('camelCasing');
+
+
+/***************
+IP validation
+***************/
+
+// Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0..255 (included).
+
+// Input to the function is guaranteed to be a single string.
+
+// Examples
+// valid inputs:
+// 1.2.3.4
+// 123.45.67.89
+
+// invalid inputs:
+// 1.2.3
+// 1.2.3.4.5
+// 123.456.78.90
+// 123.045.067.089
+// Note: leading zeros (e.g. 01.02.03.04) are considered not valid in this kata!
+
+// from google search
+function isValidIP(str) {
+  return /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/.test(str);
+}
+
+
+/***************
+Catching Car Mileage Numbers
+***************/
+
+// Let's make it so Bob never misses another interesting number. We've hacked into his car's computer, and we have a box hooked up that reads mileage numbers. We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
+
+// It's up to you, intrepid warrior, to glue the parts together. Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
+
+// Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+
+// "Interesting" Numbers
+// Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+
+// Any digit followed by all zeros: 100, 90000
+// Every digit is the same number: 1111
+// The digits are sequential, incementing†: 1234
+// The digits are sequential, decrementing‡: 4321
+// The digits are a palindrome: 1221 or 73837
+// The digits match one of the values in the awesomePhrases array
+// † For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
+// ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+
+// So, you should expect these inputs and outputs:
+
+// "boring" numbers
+// isInteresting(3, [1337, 256]);    // 0
+// isInteresting(3236, [1337, 256]); // 0
+
+// progress as we near an "interesting" number
+// isInteresting(11207, []); // 0
+// isInteresting(11208, []); // 0
+// isInteresting(11209, []); // 1
+// isInteresting(11210, []); // 1
+// isInteresting(11211, []); // 2
+
+// nearing a provided "awesome phrase"
+// isInteresting(1335, [1337, 256]); // 1
+// isInteresting(1336, [1337, 256]); // 1
+// isInteresting(1337, [1337, 256]); // 2
+// Error Checking
+// A number is only interesting if it is greater than 99!
+// Input will always be an integer greater than 0, and less than 1,000,000,000.
+// The awesomePhrases array will always be provided, and will always be an array, but may be empty. (Not everyone thinks numbers spell funny words...)
+// You should only ever output 0, 1, or 2.
+
+const incrementingCheck = (numString) => {
+  let prev = 0;
+  for (var i  = 1; i < numString.length; i++) {
+    if(parseInt(numString[prev]) === 9) {
+      if(parseInt(numString[i]) !== 0) {
+        return false;
+      }
+    } else if(parseInt(numString[i]) !== parseInt(numString[prev]) + 1) {
+      return false;
+    }
+    prev++;
+  }
+  return true;
+};
+
+const decrementingCheck = (numString) => {
+  let prev = 0;
+  for (var i  = 1; i < numString.length; i++) {
+    if(parseInt(numString[i]) !== parseInt(numString[prev]) - 1) {
+      return false;
+    }
+    prev++;
+  }
+  return true;
+};
+
+const isInteresting = (number, awesomePhrases) => {
+  // checks to see if number is less than 98
+  if(number < 98) {
+    return 0;
+  }
+
+  if(number === 98 || number === 99) {
+    return 1;
+  }
+
+  let numberPlus1 = number + 1;
+  let numberPlus2 = number + 2;
+  
+  //checking to see if number is all repeated digits
+  let numberString = number.toString();
+  let numberPlus1String = (numberPlus1).toString();
+  let numberPlus2String = (numberPlus2).toString();
+  console.log('repeat', /^(.)\1+$/.test(numberString))
+  if(/^(.)\1+$/.test(numberString)) {
+    return 2;
+  }
+  if(/^(.)\1+$/.test(numberPlus1String) || /^(.)\1+$/.test(numberPlus2String)) {
+    return 1;
+  }
+
+  // checks to see if number is a digit followed by zeros
+  let tenPower = number.toString().length;
+  let tenPowered = Math.pow(10, tenPower-1);
+  let zerocheck = number % tenPowered;
+  if(zerocheck === 0) {
+    return 2;
+  } else if(numberPlus1 % tenPowered === 0 || numberPlus2 % tenPowered === 0) {
+    return 1;
+  }
+
+
+  // test awesome array
+  for (let i = 0; i < awesomePhrases.length; i++) {
+    if(number === awesomePhrases[i]) {
+      return 2;
+    } else if (numberPlus1 === awesomePhrases[i] || numberPlus2 === awesomePhrases[i]) {
+      return 1;
+    }
+  }
+
+  // test incrementing
+  if (incrementingCheck(numberString)) {
+    return 2;
+  } else if(incrementingCheck(numberPlus1String) || incrementingCheck(numberPlus2String)) {
+    return 1;
+  }
+  // test decrementing
+  if (decrementingCheck(numberString)) {
+    return 2;
+  } else if(decrementingCheck(numberPlus1String) || decrementingCheck(numberPlus2String)) {
+    return 1;
+  }
+
+  // checks palindrome
+  if(numberString === numberString.split('').reverse().join('')) {
+    return 2;
+  } else if(numberPlus1String === numberPlus1String.split('').reverse().join('') || numberPlus2String === numberPlus2String.split('').reverse().join('')) {
+    return 1;
+  }
+
+  return 0;
+};
+
+
+// best practices answer:
+
+// function isInteresting(number, awesomePhrases) {
+//   var tests = [
+//     function(n) { return /^\d00+$/.test(n); },
+//     function(n) { return /^(\d)\1+$/.test(n); },
+//     function(n) { return RegExp(n).test(1234567890); },
+//     function(n) { return RegExp(n).test(9876543210); },
+//     function(n) { return n + '' == (n + '').split('').reverse().join(''); },
+//     function(n) { return awesomePhrases.some(function(p) { return p == n; }); }
+//   ];
+  
+//   var interesting = 0;
+//   tests.some(function(test) {
+//     if (number > 99 && test(number))
+//       return interesting = 2;
+//     else if ((number > 98 && test(number + 1)) || (number > 97 && test(number + 2)))
+//       interesting = 1;
+//   });
+//   return interesting;
+// }
+
